1f2afbf0db39dc67cb2c5f7e402e49d95c8217a3
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2019-01-13 15:53:58 +0100

    Add tracing of arrays nested into structs.

--- include/klee/klee.h
+++ include/klee/klee.h
@@ -243,15 +243,23 @@ extern "C" {
   void klee_trace_param_ptr_nested_field_directed(void* ptr, int base_offset,
                                                   int offset, int width, char* name,
                                                   TracingDirection td);
+  void klee_trace_param_ptr_nested_field_arr_directed(void* ptr, int base_offset,
+                                                      int offset, int el_width,
+                                                      int count, char* name,
+                                                      TracingDirection td);
   void klee_trace_ret_ptr_nested_field(int base_offset,
                                        int offset, int width, char* name);
-  void klee_trace_extra_ptr(void* ptr, int width, char* name, char* type, TracingDirection td);
+  void klee_trace_extra_ptr(void* ptr, int width, char* name,
+                            const char* type, TracingDirection td);
   void klee_trace_extra_ptr_arr(void* ptr, int elm_width, int count, char* name, char* type, TracingDirection td);
   void klee_trace_extra_ptr_field(void* ptr, int offset, int width, char* name, TracingDirection td);
   void klee_trace_extra_ptr_field_just_ptr(void* ptr, int offset,
                                            int width, char* name);
   void klee_trace_extra_ptr_nested_field(void* ptr, int base_offset,
                                          int offset, int width, char* name, TracingDirection td);
+  void klee_trace_extra_ptr_nested_field_arr(void* ptr, int base_offset,
+                                             int offset, int el_width, int count,
+                                             const char* name, TracingDirection td);
   void klee_trace_extra_ptr_nested_nested_field(void* ptr, int base_base_offset,
                                                 int base_offset, int offset,
                                                 int width, char* name, TracingDirection td);
